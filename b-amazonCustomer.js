1b5b2cde680bcd43e0decfad773c1e5537c33489
Zaldana <alexandrazaldana@gmail.com>
2019-03-12 18:46:14 -0700

    product id works

--- b-amazonCustomer.js
+++ b-amazonCustomer.js
@@ -4,23 +4,35 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-   
+
     // Your username
     user: "root",
-   
+
     // Your password
     password: "al3xZa1!",
     database: "bamazon"
-   });
-   
-   connection.connect(function(err) {
+});
+
+connection.connect(function (err) {
     if (err) throw err;
     readProducts();
-   });
+});
 
-   function readProducts() {
+//    function runSearch(){
+//        inquirer.prompt({
+//            name: "action",
+//            type: "input",
+//            message: "Please enter the ID of the product you would like to buy: "
+//        }).then(function(answer){
+//            console.log(answer);
+//            readProducts();
+//        })
+//    }
+
+
+function readProducts() {
     console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function (err, res) {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
         console.table(res);
@@ -30,19 +42,57 @@ var connection = mysql.createConnection({
             name: "action",
             type: "input",
             message: "Please enter the ID of the product you would like to buy: "
-        }).then(function(answer){
+        }).then(function (answer) {
 
-            var query = "SELECT product_name, price, stock_quantity FROM products WHERE ?";
-            connection.query(query, { item_id: answer.item }, function(err, res) {
+            var query = "SELECT item_id, product_name, price FROM products WHERE ?";
+            connection.query(query, { item_id: answer.action }, function (err, res) {
                 if (err) throw err;
                 for (var i = 0; i < res.length; i++) {
-                  console.log("Product: " + res[i].product_name);
+                    // console.table(res[i].item_id + res[i].product_name);
                 }
+                console.table(res);
+                askQuantity();
+                connection.end();
+
+                // runSearch();
+
             });
         })
     });
     // searchID();
 }
+
+function askQuantity() {
+    inquirer.prompt({
+        name: "quantity",
+        type: "input",
+        message: "Please enter quantity: "
+    }).then(function (answer) {
+        var userInput;
+
+        // for (var i =0; i < answer.length; i++){
+        //     if (hhh) {
+
+        //     }
+        // }
+        var query = connection.query("UPDATE products SET ? WHERE ?",
+        [{
+            stock_quantity:answer.quantity,
+        },
+        {
+            item_id:answer.item_id
+        }
+        ],
+
+        function(err) {
+            if (err) throw err;
+            console.log("Item added successfully");
+            readProducts();
+        })
+    });
+}
+
+
 
 // function searchID(){
 //     inquirer.prompt({
@@ -72,7 +122,7 @@ var connection = mysql.createConnection({
 //           "Find all artists who appear more than once",
 //           "Find data within a specific range",
 //           "Search for a specific song"
-          
+
 //         ]
 //       })
 //       .then(function(answer) {
@@ -80,15 +130,15 @@ var connection = mysql.createConnection({
 //           case "Find songs by artist":
 //             artistSearch();
 //             break;
-   
+
 //           case "Find all artists who appear more than once":
 //             multiSearch();
 //             break;
-   
+
 //           case "Find data within a specific range":
 //             rangeSearch();
 //             break;
-   
+
 //           case "Search for a specific song":
 //             songSearch();
 //             break;
