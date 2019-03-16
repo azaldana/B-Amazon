5baf88cfb3416e7ef265a8fda4ffaf1f52d713de
Zaldana <alexandrazaldana@gmail.com>
2019-03-15 17:31:04 -0700

    final edits

--- b-amazonCustomer.js
+++ b-amazonCustomer.js
@@ -18,17 +18,6 @@ connection.connect(function (err) {
     readProducts();
 });
 
-//    function runSearch(){
-//        inquirer.prompt({
-//            name: "action",
-//            type: "input",
-//            message: "Please enter the ID of the product you would like to buy: "
-//        }).then(function(answer){
-//            console.log(answer);
-//            readProducts();
-//        })
-//    }
-
 
 function readProducts() {
     console.log("Selecting all products...\n");
@@ -51,8 +40,8 @@ function readProducts() {
                     // console.table(res[i].item_id + res[i].product_name);
                 }
                 console.table(res);
-                askQuantity();
-                connection.end();
+                askQuantity(answer.action);
+                // connection.end();
 
                 // runSearch();
 
@@ -62,86 +51,83 @@ function readProducts() {
     // searchID();
 }
 
-function askQuantity() {
+function askQuantity(item_id) {
     inquirer.prompt({
         name: "quantity",
         type: "input",
         message: "Please enter quantity: "
-    }).then(function (answer) {
-        var userInput;
-
-        // for (var i =0; i < answer.length; i++){
-        //     if (hhh) {
-
-        //     }
-        // }
-        var query = connection.query("UPDATE products SET ? WHERE ?",
-        [{
-            stock_quantity:answer.quantity,
-        },
-        {
-            item_id:answer.item_id
-        }
-        ],
-
-        function(err) {
+    }).then(function (user) {
+        connection.query("SELECT stock_quantity, price FROM products WHERE ?", { item_id: parseInt(item_id) }, function (err, res) {
+            // console.log(res);
             if (err) throw err;
-            console.log("Item added successfully");
-            readProducts();
-        })
-    });
+            for (var i = 0; i < res.length; i++) {
+                // console.table(res[i].item_id + res[i].product_name);
+            }
+
+            // console.table(res);
+            var currentStock = res[0].stock_quantity;
+            var itemPrice = res[0].price;
+            // console.log(res[0].price);
+            // console.log(currentStock);
+
+            if (user.quantity > currentStock) {
+                console.log("I'm sorry but this product is currently out of stock.");
+                readProducts();
+
+              } else {
+                console.log("This item has been successfully added to your cart. ");
+                // console.log("Quantity: " + res[0].stock_quantity - user.quantity);
+                // console.log("----------------");
+                updatedQ= parseInt(currentStock) - parseInt(user.quantity);
+                // console.log(updatedQ);
+                connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [updatedQ , item_id ], function (err, res) {
+                    // console.log("stock has been updated");
+                    console.log("Your final total is: " + user.quantity * itemPrice);
+                    connection.end();
+                })
+            }
+        });
+    })
 }
 
+// function updateQuantity(key){
+//     var quantityEntered = [];
+//     var userInput = [];
+//     var finalPrice = price * quantityEntered;
+//     var price = res[0].price;
 
+//     key.userInput = quantityEntered;
+//     quantityEntered.push(key.userInput);
 
-// function searchID(){
-//     inquirer.prompt({
-//         name: "action",
-//         type: "input",
-//         message: "Please enter the ID of the product you would like to buy: "
-//     }).then(function(answer){
-//         connection.query("SELECT item_id * FROM products", function (err, res) {
-//             if (err) throw err;
-//             // Log all results of the SELECT statement
-//             console.table(res);
-//             connection.end();
-//         });
-//     })
+//     var query = connection.query(
+//         "UPDATE stock_quantity FROM products WHERE ?",
+        
+//             {
+//                 stock_quantity: 2500
+//             },
+
+//         function (err, res) {
+//             console.log(res.affectedRows + " item updated!\n");
+//             // Call deleteProduct AFTER the UPDATE completes
+//             // deleteProduct();
+//         }
+//     );
 
 // }
 
+// function buyMore(){
+//     inquirer.prompt({
+//         name: "stop",
+//         type: "confirm",
+//         message: "Would you like to buy another item?"
+//     }).then(function (response) {
+//         if (response.stop){
+//             readProducts();
 
-//    function searchProducts() {
-//     inquirer
-//       .prompt({
-//         name: "action",
-//         type: "list",
-//         message: "What would you like to do?",
-//         choices: [
-//           "Find songs by artist",
-//           "Find all artists who appear more than once",
-//           "Find data within a specific range",
-//           "Search for a specific song"
-
-//         ]
-//       })
-//       .then(function(answer) {
-//         switch (answer.action) {
-//           case "Find songs by artist":
-//             artistSearch();
-//             break;
-
-//           case "Find all artists who appear more than once":
-//             multiSearch();
-//             break;
-
-//           case "Find data within a specific range":
-//             rangeSearch();
-//             break;
-
-//           case "Search for a specific song":
-//             songSearch();
-//             break;
+//         } else {
+//             console.log("Thank you for shopping. Your final total comes out to:");
+//             connection.end();
 //         }
-//       });
-//    }
+        
+//     })
+// }
